54d7413e2fa8551697bf6ce23d8464a312daf660
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-10-27 14:14:28 -0400

    Properly encoding infectedIDs, created locally/not grabbing data from auth or infection api, into FCM request

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -8,8 +8,9 @@ class NotificationsController < ApplicationController
 
         device_tokens.each do |device_token|
             send_notification(device_token)
-         end
-        
+        end
+        render json: {status: "Successfully sent notifications"}
+
     end
 
     private 
@@ -43,20 +44,22 @@ class NotificationsController < ApplicationController
     end
 
     def send_fcm_notification(device_token)
+        # Get the infected IDs and their dates
+        rawInfectedIDs = [{"date":"2020-10-27T17:35:19.827Z","tempID":"092830j209f2"},{"date":"2020-10-27T17:35:19.827Z","tempID":"weokeokwpowe"}]
+        # Format for embedding into request
+        infectedIDs = JSON.generate(rawInfectedIDs) # => "{\"hello\":\"goodbye\"}"
+        # Assemble Request
         url = URI("https://fcm.googleapis.com/fcm/send")
-
         https = Net::HTTP.new(url.host, url.port);
         https.use_ssl = true
-        
         request = Net::HTTP::Post.new(url)
         request["Authorization"] = "key=AAAAQPfWnqU:APA91bELotc45F69FyZUtQL5A4NnrIVwS-CsiMOE2OaWWgrcf53v3tvrbVdkZoL-b7ApjfgygOdN3Dd8neo45NGnpIhof8WfQ1pllAxXv3DWL3nVu1x36oOVnrTL09AH0sc9CnfRMir1"
         request["Content-Type"] = "application/json"
         request["Host"] = "fcm.googleapis.com"
-        request.body = "{\n    \"to\":\"#{device_token}\",\n    \"notification\" : {\n     \"body\" : \"please work\",\n     \"title\": \"Notification from postman\"\n    },\n    \"data\" : {\n        \"body\" : \"please work\",\n        \"title\": \"Notification from postman\",\n        \"key_1\" : \"Value for key_1\",\n        \"key_2\" : \"Value for key_2\"\n    }\n}"
-        
+        request.body = "{\n    \"to\":\"#{device_token}\",\n    \"notification\" : {\n     \"body\" : \"please work\",\n     \"title\": \"Notification from postman\"\n    },\n    \"data\" : {\n        \"body\" : \"please work\",\n        \"title\": \"Notification from postman\",\n        \"infectedIDs\": #{infectedIDs}  }\n}"
+        # Send Request
         response = https.request(request)
-
-        render json: response.read_body
+        # Send Feedback
     end
 
 end
