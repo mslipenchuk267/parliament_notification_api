ac0e4e030978dcbc797cbbdb876b2a953ea5020a
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-10-27 13:25:38 -0400

    Added rpush gem, got apns notifications working

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_215436) do
+ActiveRecord::Schema.define(version: 2020_10_27_165339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "infections", force: :cascade do |t|
-    t.string "temp_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "notifications", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
