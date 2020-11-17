3f77669bb9f925dff8cd309cb646c340f6d1a497
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-11-17 16:06:10 -0500

    Added code to split apart the infectedIDs if they are too large.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,14 +5,17 @@ class NotificationsController < ApplicationController
 
     def test_notification
         # Get  infectedIDs (temp_id, created_date) from Infection API
-        @infectedIDs = get_infected_ids
+        infectedIDs = get_infected_ids
+        payloadChunks = infectedIDs.each_slice(19)
 
         # Get deviceKeys from Auth API
         device_tokens = get_device_tokens
         
         # Send notifications to all users
         device_tokens.each do |device_token|
-            send_notification(device_token)
+            payloadChunks.each do |payloadChunk|
+                send_notification(device_token, JSON.generate(payloadChunk))
+            end
         end
         render json: {status: "Successfully sent notifications"}
 
@@ -21,7 +24,7 @@ class NotificationsController < ApplicationController
     private 
 
     def get_infected_ids
-        url_string = ENV['INFECTION_URI'] + "/temp_ids"
+        url_string = "http://a73906904480049e69678e0cb9be2e22-1728580132.us-east-2.elb.amazonaws.com" + "/temp_ids"
         url = URI(url_string)
         http = Net::HTTP.new(url.host, url.port);
         request = Net::HTTP::Post.new(url)
@@ -34,11 +37,10 @@ class NotificationsController < ApplicationController
         rawInfectedIDs = result_json['temp_ids']
         # Remove null entries
         rawInfectedIDs -= [nil]
-        JSON.generate(rawInfectedIDs)
     end
 
     def get_device_tokens
-        url_string = ENV['AUTH_URI'] + "/device_keys"
+        url_string = "http://a87713a1fd4b64cd4b788e8a1592de07-1206905140.us-west-2.elb.amazonaws.com" + "/device_keys"
         url = URI(url_string)
         http = Net::HTTP.new(url.host, url.port);
         request = Net::HTTP::Post.new(url)
@@ -53,27 +55,27 @@ class NotificationsController < ApplicationController
         device_tokens -= [nil]
     end
 
-    def send_notification(device_token)
+    def send_notification(device_token, payloadChunk)
         if device_token.length < 162
-            send_apns_notification(device_token)
+            send_apns_notification(device_token, payloadChunk)
         else 
-            send_fcm_notification(device_token)
+            send_fcm_notification(device_token, payloadChunk)
         end
     end
 
-    def send_apns_notification(device_token)
+    def send_apns_notification(device_token, payloadChunk)
         # Create notification
         n = Rpush::Apns::Notification.new
-        n.app = Rpush::Apns::App.find_by_name("parliament_ios")
+        n.app = Rpush::Apns::App.find_by_name("parliament_ios_prod_test")
         n.device_token = device_token
         n.alert = {
-            title: "From Notification API",
-            body: "Hello World 2"
+            title: "Parliament",
+            body: "Check the Notifications Tab!"
         }
         # pass any custom data here
         n.data = {
             type: 'message',
-            infectedIDs: @infectedIDs,
+            infectedIDs: payloadChunk,
         }
         n.sound = "water_droplet_3.wav"
         n.content_available = true
@@ -82,7 +84,7 @@ class NotificationsController < ApplicationController
         Rpush.push
     end
 
-    def send_fcm_notification(device_token)
+    def send_fcm_notification(device_token, payloadChunk)
         # Assemble Request
         url = URI("https://fcm.googleapis.com/fcm/send")
         https = Net::HTTP.new(url.host, url.port);
@@ -91,10 +93,9 @@ class NotificationsController < ApplicationController
         request["Authorization"] = "key=AAAAQPfWnqU:APA91bELotc45F69FyZUtQL5A4NnrIVwS-CsiMOE2OaWWgrcf53v3tvrbVdkZoL-b7ApjfgygOdN3Dd8neo45NGnpIhof8WfQ1pllAxXv3DWL3nVu1x36oOVnrTL09AH0sc9CnfRMir1"
         request["Content-Type"] = "application/json"
         request["Host"] = "fcm.googleapis.com"
-        request.body = "{\n    \"to\":\"#{device_token}\",\n    \"notification\" : {\n     \"body\" : \"please work\",\n     \"title\": \"Notification from postman\"\n    },\n    \"data\" : {\n        \"body\" : \"please work\",\n        \"title\": \"Notification from postman\",\n        \"infectedIDs\": #{@infectedIDs}  }\n}"
+        request.body = "{\n    \"to\":\"#{device_token}\",\n    \"notification\" : {\n     \"body\" : \"please work\",\n     \"title\": \"Notification from postman\"\n    },\n    \"data\" : {\n        \"body\" : \"please work\",\n        \"title\": \"Notification from postman\",\n        \"infectedIDs\": #{payloadChunk}  }\n}"
         # Send Request
         response = https.request(request)
-        # Send Feedback
     end
 
 end
